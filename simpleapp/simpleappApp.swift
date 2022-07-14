643b0a34b57b0b0e820f22ca87550e093dafb451
nuthan <nuthan rao>
2022-07-14 18:15:08 -0400

    Initial Commit

---
+++ simpleapp/simpleappApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleappApp.swift
+//  simpleapp
+//
+//  Created by Nuthan Rao on 2022-07-14.
+//
+
+import SwiftUI
+
+@main
+struct simpleappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
